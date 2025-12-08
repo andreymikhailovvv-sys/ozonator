b762b3dad7efacb16dc8a7e8536d0ebfd5d668fe
andreymikhailovvv-sys <andreymikhailovvv@gmail.com>
2025-12-08 18:25:56 +0300

    Update telegram_api.py

--- telegram_api.py
+++ telegram_api.py
@@ -2,10 +2,20 @@ import requests
 import os
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
-BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+BASE = f"https://api.telegram.org/bot{TOKEN}"
 
 def send_message(chat_id, text):
-    url = f"{BASE_URL}/sendMessage"
-    payload = {"chat_id": chat_id, "text": text}
-    r = requests.post(url, json=payload)
-    return r.status_code
+    requests.post(f"{BASE}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": text
+    })
+
+
+def send_keyboard(chat_id, text, keyboard):
+    requests.post(f"{BASE}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": text,
+        "reply_markup": {
+            "inline_keyboard": keyboard
+        }
+    })
